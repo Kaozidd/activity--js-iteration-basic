e4fd40f21b026bea488a725ac2a1eff81efd66df
kaozidd <arthur_hemingway@outlook.com>
2018-02-21 11:22:51 -0600

    Morning Activity

--- ex-01-hasQ.js
+++ ex-01-hasQ.js
@@ -6,17 +6,51 @@
 //   NOTE: You must build the function so that it can detect capital letters
 //------------------------------------------------------------------
 
+// function hasQ(string) {
+// 	let bool = false;
+// 	for (let i=0; i<string.length; i++) {
+// 		if (string[i] === 'q' || string[i] === 'Q') {
+// 			bool = true;
+// 		}
+// 	}
+// 	return bool;
+// }
+
+
+
+
+// function hasQ(string) {
+// 	let bool = false;
+// 	for (let i=0; i<string.length; i++) {
+// 		if (string[i].toUpperCase() === 'Q') {
+// 			bool = true;
+// 		}
+// 	}
+// 	return bool;
+// }
+
+
+
+function hasQ(string) {
+	let bool = false;
+	for (let i=0; i<string.length; i++) {
+		if (string[i].toLowerCase() === 'q') {
+			bool = true;
+		}
+	}
+	return bool;
+}
 
 var output1 = hasQ("my mother enjoys quilting.")
 var output2 = hasQ("i'm not wearing any socks.")
-var output4 = hasQ("I went to a quarry to pick up some stone slabs.")
-var output5 = hasQ("My brain is made of cheese and my knees are jelly")
-var output3 = hasQ("Quit yelling in my ear.")
-var output3 = hasQ("My best friend's name is Quincy.")
+var output3 = hasQ("I went to a quarry to pick up some stone slabs.")
+var output4 = hasQ("My brain is made of cheese and my knees are jelly")
+var output5 = hasQ("Quit yelling in my ear.")
+var output6 = hasQ("My best friend's name is Quincy.")
 
 console.assert(output1 === true )
 console.assert(output2 === false )
 console.assert(output3 === true)
 console.assert(output4 === false)
 console.assert(output5 === true)
-console.assert(output5 === true)
+console.assert(output6 === true)
--- ex-02-getAllGreaterThan.js
+++ ex-02-getAllGreaterThan.js
@@ -17,6 +17,17 @@
 //------------------------------------------------------------------
 
 
+function getAllGreaterThan(n, arr) {
+	const newArr = [];
+	for (let i=0; i<arr.length; i++) {
+		if (arr[i] > n) {
+			newArr.push(arr[i]);
+		}
+	}
+	return newArr;
+}
+
+
 // *** DONT TOUCH ***
 
 var outputArray_1 = getAllGreaterThan(5, [1,2,3,4,5,6,7,8])
@@ -29,9 +40,9 @@ console.assert(outputArray_1[1] === 7)
 var exampleArray_2 = [10,12,30,4,2,16,7,88]
 var outputArray_2 = getAllGreaterThan(10, exampleArray_2 )
 console.assert(outputArray_2.length === 4 )
-console.assert(outputArray_1[0] === 12)
-console.assert(outputArray_1[1] === 30)
-console.assert(outputArray_1[3] === 88)
+console.assert(outputArray_2[0] === 12)
+console.assert(outputArray_2[1] === 30)
+console.assert(outputArray_2[3] === 88)
 
 
 var exampleArray_3 = [10,12,30,4,2,16,7,88]
